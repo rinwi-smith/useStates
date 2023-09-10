b04d5fc82cecbdc86432a97770434f7f976a2aa8
Rinwi Smith <72204068+rinwi-smith@users.noreply.github.com>
2023-09-10 16:16:11 +0100

    h1

--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -9,6 +9,7 @@ function Input() {
     }
   return (
     <div className='mt-10 bg-gray-300' >
+    <h1>Input Field</h1>
         <input className=' bg-gray-200 border-solid border-[#000 !important' type="text" value={inputValue} onChange={handleChange} placeholder='type here' />
         <p>Typed value: {inputValue} </p>
     </div>
